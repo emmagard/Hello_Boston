c550519a192402adfdfe42cce9c489fcf70f0479
Emma Gardner <emmagardner@outlook.com>
2014-07-08 14:01:45 -0400

    I added an initializer file for instagram. I'm starting to get the hang of the using the instagram gem although it's mostly magic at this point...

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,8 @@
+
+
+Instagram.configure do |config|
+  config.client_id = "5020b7330236467198e55069a6de61b7"
+  config.client_secret = "281a41d90d3a46989c62095135383664"
+  # For secured endpoints only
+  #config.client_ips = '<Comma separated list of IPs>'
+end
