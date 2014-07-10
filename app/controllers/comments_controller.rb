1e3f171a75946f9750d473df0ca68a4b58a05210
Emma Gardner <emmagardner@outlook.com>
2014-07-10 13:04:27 -0400

    comments form functions and does what i want it to

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,32 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @post = Post.find_by(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comments = @post.comments
   end
+
+  def new
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    #pushes the new comment into posts.comments
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      flash[:notice] = "Your Comment Did not Save"
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit([:content])
+  end
+
 end
