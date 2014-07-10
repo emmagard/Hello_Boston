b5bbd3a3c5e732195c133aa1b50e8049a69f1e29
Emma Gardner <emmagardner@outlook.com>
2014-07-10 09:24:37 -0400

    added notes to indicate where I made changes to customize devise with :username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ActiveRecord::Base
+  #added to customize :username in devise
+  attr_accessor :login
+
   has_many :posts
   validates :username, presence: true
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, case_sensitive: false
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  #added to customize :username in devise
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      else
+        where(conditions).first
+      end
+    end
 
 end
