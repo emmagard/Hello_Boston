f2f45b948dbba529b1457088059af6321960ceba
Emma Gardner <emmagardner@outlook.com>
2014-08-03 20:12:43 -0400

    added keys to .env file

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,8 +1,8 @@
 
 
 Instagram.configure do |config|
-  config.client_id = "5020b7330236467198e55069a6de61b7"
-  config.client_secret = "281a41d90d3a46989c62095135383664"
+  config.client_id = ENV['INSTAGRAM_API_CLIENT_ID']
+  config.client_secret = ENV['INSTAGRAM_API_CLIENT_SECRET']
   # For secured endpoints only
   #config.client_ips = '<Comma separated list of IPs>'
 end
