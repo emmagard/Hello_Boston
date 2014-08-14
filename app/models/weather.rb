1437a5814a8bd5d7b975e9b7be738d36214cc408
Emma Gardner <emmagardner@outlook.com>
2014-08-14 14:04:09 -0400

    refactored views for comment new and comment edit

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,11 +3,15 @@ class Weather
   @w_api = Wunderground.new
 
   def self.boston_conditions
-    @w_api.conditions_for("02116")["current_observation"]
+    Rails.cache.fetch('boston_conditions', expires_in: 30.minutes) do
+      @w_api.conditions_for("02116")["current_observation"]
+    end
   end
 
   def self.boston_forecast
-    @w_api.forecast_for("MA", "Boston")["forecast"]["simpleforecast"]["forecastday"]
+    Rails.cache.fetch('boston_forecast', expires_in: 30.minutes) do
+      @w_api.forecast_for("MA", "Boston")["forecast"]["simpleforecast"]["forecastday"]
+    end
   end
 
 end
