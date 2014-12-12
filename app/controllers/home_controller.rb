876cddb4e7f191bc5fd0325847ac388669d810d5
Emma Gardner <emmagardner@outlook.com>
2014-12-11 20:29:25 -0500

    added styling changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @feed = Feedjira::Feed.fetch_and_parse feed_url
-    @entry = @feed.entries
-    @pics = Instagram.tag_recent_media('boston').values_at(0...15)
+    @entry = @feed.entries.values_at(0...5)
+    @pics = Instagram.tag_recent_media('boston').values_at(0...9)
     @posts = Post.limit(3).order(created_at: :desc)
     @post = Post.find_by(params[:id])
 
